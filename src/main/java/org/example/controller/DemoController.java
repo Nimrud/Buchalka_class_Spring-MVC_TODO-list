b010592db056f240a2942850575e9f5b38fa3677
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-08-12 02:37:23 +0200

    07: challenge 2

--- src/main/java/org/example/controller/DemoController.java
+++ src/main/java/org/example/controller/DemoController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DemoController {
 
+    // == fields ==
+    private final DemoService demoService;
+
+    // == constructors ==
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // == request methods ==
     @ResponseBody
     @GetMapping("/hello")         // http://localhost:8080/TODO-list/hello
     public String hello() {
@@ -19,16 +31,17 @@ public class DemoController {
 
     @GetMapping("welcome")
     public String welcome(Model model) {
-        model.addAttribute("user", "Chris");
+        model.addAttribute("newUser", demoService.getHelloMessage("Mark"));
         log.info("model = {}", model);
         return "welcome";
         // dzięki użyciu kodu w klasie WebConfig, prefiks i sufiks zostaną automatycznie dodane
         // więc możemy tu wpisać samo 'welcome' zamiast '/WEB-INF/view/welcome.jsp'
     }
 
+    // == model attributes ==
     @ModelAttribute("welcomeMessage")
     public String welcomeMessage() {
         log.info("welcomeMessage() called");
-        return "Welcome to this Demo Application";
+        return demoService.getWelcomeMessage();
     }
 }
