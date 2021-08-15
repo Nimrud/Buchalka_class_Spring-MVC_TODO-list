8920e5d785e4c93ef8f27b81a1aeb70f7a7ffbf6
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-08-15 03:50:24 +0200

    10: podstawowy kontroler aplikacji

---
+++ src/main/java/org/example/util/Mappings.java
@@ -0,0 +1,10 @@
+package org.example.util;
+
+public final class Mappings {
+
+    // == constants ==
+    public static final String ITEMS = "items";
+
+    // == constructors ==
+    private Mappings() {}
+}
