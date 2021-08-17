c9aaaca8f09667388f417fc0fe16305f10c19a6f
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-08-17 02:21:00 +0200

    13: formularz dodawania nowej pozycji do listy (1)

--- src/main/java/org/example/controller/TodoItemController.java
+++ src/main/java/org/example/controller/TodoItemController.java
@@ -1,19 +1,22 @@
 package org.example.controller;
 
 import org.example.model.TodoData;
+import org.example.model.TodoItem;
 import org.example.service.TodoItemService;
+import org.example.util.AttributeNames;
 import org.example.util.Mappings;
 import org.example.util.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoItemController {
 
     // == fields ==
-    private TodoItemService todoItemService;
+    private final TodoItemService todoItemService;
 
     // == constructors ==
     @Autowired
@@ -31,5 +34,10 @@ public class TodoItemController {
     @GetMapping(Mappings.ITEMS)     // http://localhost:8080/TODO-list/items
     public String items() {
         return ViewNames.ITEMS_LIST;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem item) {
+        return "redirect:/" + Mappings.ITEMS;
     }
 }
